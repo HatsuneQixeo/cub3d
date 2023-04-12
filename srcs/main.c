ff7370bbf4c385543fdb9f1b341ecf64e26a40a1
Hatsune Qixeo <qiyong831@gmail.com>
2023-04-13 03:25:18 +0800

    a little bit of cleanup in main

--- srcs/main.c
+++ srcs/main.c
@@ -26,11 +26,6 @@ void	point_log(const char *prefix, const t_point point)
 	printf("%sx: %f, y: %f\n", prefix, point.x, point.y);
 }
 
-int	point_equal(const t_point point1, const t_point point2)
-{
-	return (point1.x == point2.x && point1.y == point2.y);
-}
-
 void	point_move(t_point *point, const t_point vector)
 {
 	const double	speed = 6;
@@ -54,15 +49,10 @@ int	display_mouse(const t_mouse mouse)
 	const int	in_screen = (unsigned int)mouse.pos.x < ScreenWidth
 		&& (unsigned int)mouse.pos.y < ScreenHeight;
 
-	if (in_screen && !custom_cursor)
+	if (in_screen != custom_cursor)
 	{
-		mlx_mouse_hide();
-		custom_cursor = 1;
-	}
-	else if (!in_screen && custom_cursor)
-	{
-		mlx_mouse_show();
-		custom_cursor = 0;
+		custom_cursor = in_screen;
+		(int (*[])(void)){mlx_mouse_show, mlx_mouse_hide}[custom_cursor]();
 	}
 	return (custom_cursor);
 }
@@ -95,7 +85,10 @@ void	start_to_end(t_point *start, t_point *end,
 void	cast_a_ray(const t_mlx mlx, const t_point player, const t_point cursor)
 {
 	const t_point	direction = point_sub(player, cursor);
-	const t_point	size = point_round((t_point){.x = ft_max(fabs(direction.x), 1), .y = ft_max(fabs(direction.y), 1)}, round);
+	const t_point	size = (t_point){
+		.x = ft_max(fabs(direction.x), 1),
+		.y = ft_max(fabs(direction.y), 1)
+	};
 	t_image			ray;
 	t_point			start;
 	t_point			end;
@@ -106,8 +99,8 @@ void	cast_a_ray(const t_mlx mlx, const t_point player, const t_point cursor)
 	ray.putoffset_y = offset_direction(direction.y);
 	start_to_end(&start, &end, size, direction);
 	image_clear(ray);
-	image_draw_line(ray, colour_from_percentage(.9, .2, .1, .4), start, end);
-	image_put(mlx, ray, point_round(player, round));
+	image_draw_line(ray, colour_from_rgba(255, 120, 100, 20), start, end);
+	image_put(mlx, ray, player);
 }
 
 void	test_rotate(const t_point dir)
@@ -123,11 +116,19 @@ void	test_rotate(const t_point dir)
 // ft_printf("くるり廻る廻る廻る世界\n");
 int	hook_loop(t_game *game)
 {
-	static t_point	prev_pos;
 	const t_point	vector = player_vector(game->keys);
 
 	if (game->keys[Key_ESC] == Press)
 		hook_button_close(EXIT_SUCCESS);
+	point_move(&game->player.pos, vector);
+	image_put(game->mlx, game->screen_buffer, (t_point){0, 0});
+	if (display_mouse(game->mouse))
+	{
+		image_put(game->mlx, game->texture.mouse_icon, game->mouse.pos);
+		if (game->mouse.left_click == Press)
+			cast_a_ray(game->mlx, game->player.pos, game->mouse.pos);
+	}
+	image_put(game->mlx, game->texture.player_icon, game->player.pos);
 	// test_rotate(game->player.dir);
 	/* Show angle */
 	{
@@ -135,25 +136,18 @@ int	hook_loop(t_game *game)
 		// if (angle != 0)
 		// 	printf("angle: %f\n", angle);
 	}
-	image_put(game->mlx, game->screen_buffer, (t_point){0, 0});
-	if (display_mouse(game->mouse))
-	{
-		image_put(game->mlx, game->texture.mouse_icon, point_round(game->mouse.pos, round));
-		if (game->mouse.left_click == Press)
-			cast_a_ray(game->mlx, game->player.pos, game->mouse.pos);
-	}
 	/* Show the mouse position */
 	{
 		// point_log("Motion: ", game->mouse.pos);
 	}
 	/* Show the mouse speed */
 	{
-		// if (!point_equal(game->mouse.pos, prev_pos))
-		// 	point_log("speed: ", point_sub(game->mouse.pos, prev_pos));
+		static t_point	prev_pos;
+
+		if (ft_memcmp(&game->mouse.pos, &prev_pos, sizeof(t_point)))
+			point_log("speed: ", point_sub(game->mouse.pos, prev_pos));
+		prev_pos = game->mouse.pos;
 	}
-	prev_pos = game->mouse.pos;
-	point_move(&game->player.pos, vector);
-	image_put(game->mlx, game->texture.player_icon, point_round(game->player.pos, round));
 	/*
 		Rotation seems to be on point,
 		should be able to draw a line starting with player position,
@@ -168,13 +162,13 @@ int	main(void)
 	t_image	image;
 
 	game = game_init();
-	game.player.pos = (t_point){.x = 0, .y = 0};
+	game.player.pos = (t_point){0, 0};
 	game.player.dir = (t_point){.x = 1, .y = 0}; /* N, S, E, W */
 	/* A backgroud
 		Might come in handy if you get annoyed by the trace
 	*/
 	// image_fill(game.screen_buffer, colour_from_rgba(100, 100, 100, 0));
-	// image_put(game.mlx, game.screen_buffer, (t_point){.x = 0, .y = 0});
+	// image_put(game.mlx, game.screen_buffer, (t_point){0, 0});
 	beta_screen_buffer(game.screen_buffer);
 	events(&game);
 	mlx_loop(game.mlx.p_mlx);
