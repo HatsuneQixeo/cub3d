e39fe3457982e0fd5b59874f3fe8b11b2a93ba23
Hatsune Qixeo <qiyong831@gmail.com>
2023-05-03 23:37:57 +0800

    renamed cast_a_ray to draw_a_line

--- srcs/draw_a_line.c
+++ srcs/draw_a_line.c
@@ -25,7 +25,7 @@ static void	start_to_end(t_point *start, t_point *end,
 	}
 }
 
-void	cast_a_ray(const t_mlx mlx, const t_point pos, const t_point vector)
+void	draw_a_line(const t_mlx mlx, const t_point pos, const t_point vector)
 {
 	const t_point	size = (t_point){
 		.x = ft_max(fabs(vector.x), 1),
@@ -37,9 +37,9 @@ void	cast_a_ray(const t_mlx mlx, const t_point pos, const t_point vector)
 
 	ray = image_create(mlx.p_mlx, size,
 			offset_vector(vector.x), offset_vector(vector.y));
-	ft_assert(ray.data != NULL, "cast_a_ray: "IMAGE_CREATION_FAILED);
+	ft_assert(ray.data != NULL, "draw_a_line: "IMAGE_CREATION_FAILED);
 	start_to_end(&start, &end, size, vector);
-	image_clear(&ray);
+	image_clean(&ray);
 	image_draw_line(&ray, colour_from_rgba(255, 255, 255, 20), start, end);
 	image_put(mlx, &ray, pos);
 }
