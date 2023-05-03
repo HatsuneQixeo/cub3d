d08eae69e80e6c6e7cbb133a78f6e139571c2c3f
Hatsune Qixeo <qiyong831@gmail.com>
2023-05-03 23:48:22 +0800

    libft

--- upperCamel_to_snake.sh
+++ upperCamel_to_snake.sh
@@ -1,4 +1,4 @@
-# Back when I didn't know norminette allows enum to have upper camel case
+# Back when I didn't know norminette allows enum to have UpperCamelCase
 
 echo "UPPER_CAMEL_TO_SNAKE:"
 echo "$@" | sed 's/\([a-z]\)\([A-Z]\)/\1_\2/g' | tr '[:lower:]' '[:upper:]'
