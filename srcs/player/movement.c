39f33fbeaa8077780bb81330b9a13715007b6775
Hatsune Qixeo <qiyong831@gmail.com>
2023-05-06 06:55:11 +0800

    smoother rounding and some note

--- srcs/player/movement.c
+++ srcs/player/movement.c
@@ -48,17 +48,24 @@ t_point	player_direction(const t_keys keys)
 	return (keys_to_vector(keys, key_vectors, len));
 }
 
+/*
+	Bug: Rounds incorrectly if the player speed is too high
+	Bug: Go through walls if the player speed is too high
+
+	Why not fix it? Not that this game has any speed boost effect anyway.
+*/
 static t_point	vector_collision(const t_point start, const t_point vector,
 			char **map, const t_point mapsize)
 {
-	t_point	end;
+	const t_point	offset = point_upscale(point_sign(vector), -.000001);
+	t_point			end;
 
 	end.x = ft_dminmax(0, start.x + vector.x, mapsize.x - 1);
 	end.y = ft_dminmax(0, start.y + vector.y, mapsize.y - 1);
 	if (map[(int)start.y][(int)end.x] == '1')
-		end.x = start.x;
+		end.x = roundf(start.x) + offset.x;
 	if (map[(int)end.y][(int)start.x] == '1')
-		end.y = start.y;
+		end.y = roundf(start.y) + offset.y;
 	return (end);
 }
 
@@ -74,7 +81,7 @@ void	player_move(t_player *player, const t_point direction,
 		vector = point_upscale(rotate, .05);
 	else
 		vector = point_upscale(rotate, .1);
-	// vector = point_upscale(vector, .2);
+	// vector = point_upscale(vector, 1);
 	player->pos = vector_collision(player->pos, vector, map, mapsize);
 }
 
