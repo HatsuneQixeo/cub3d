79e1ca3635484932869de0bb746543ae9b41bf6a
Hatsune Qixeo <qiyong831@gmail.com>
2023-05-13 16:26:44 +0800

    draw_line debug utility

--- srcs/image/draw/draw_line.c
+++ srcs/image/draw/draw_line.c
@@ -13,19 +13,21 @@
 #include "image.h"
 
 void	image_draw_line(t_image *image, const t_colour colour,
-			const t_point start, const t_point end)
+			const t_point begin, const t_point end)
 {
-	const t_point	distance = point_sub(end, start);
-	const int		total_step = ft_max(fabs(distance.x), fabs(distance.y));
-	const t_point	step_length = point_downscale(distance, total_step);
+	const t_point	vector = point_sub(end, begin);
+	const int		total_step = ft_max(fabs(vector.x), fabs(vector.y));
+	const t_point	step_length = point_downscale(vector, total_step);
 	int				step;
 	t_point			it;
 
 	step = -1;
-	it = start;
+	if (isnan(step_length.x) || isnan(step_length.y))
+		return (point_log("image_draw_line: step_length is NaN", step_length));
+	it = begin;
 	while (++step < total_step)
 	{
-		image_setpixel(image, colour, it);
+		image_setpixel(image, colour, point_apply(it, trunc));
 		it = point_add(it, step_length);
 	}
 }
