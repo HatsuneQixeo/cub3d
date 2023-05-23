3e9f9b92e541f691dba8fec1190b0fc0f50f9c0b
Hatsune Qixeo <qiyong831@gmail.com>
2023-05-24 02:03:51 +0800

    separated draw_circle and draw_filled_circle

--- srcs/image/draw/draw_circle.c
+++ srcs/image/draw/draw_circle.c
@@ -56,33 +56,14 @@ void	image_draw_circle(t_image *image, const t_colour colour,
 			point.y--;
 		}
 	}
-	if (radius > 1)
-		image_draw_circle(image, colour, center, radius - 1);
 }
 
-void	draw_filled_circle(t_image *image, const t_colour colour,
-			const t_point center, const unsigned int radius)
+void	image_draw_filled_circle(t_image *image, const t_colour colour,
+				const t_point center, const unsigned int radius)
 {
-	t_point	index;
+	unsigned int	i;
 
-	index.y = center.y - radius - 1;
-	while (++index.y < center.y + radius)
-	{
-		index.x = center.x - radius - 1;
-		while (++index.x < center.x + radius)
-		{
-			if (point_distance(index, center) <= radius)
-				image_setpixel(image, colour, index);
-		}
-	}
-}
-
-void	image_fill_circle(t_image *image, const t_colour colour)
-{
-	const t_point	center = point_apply(point_upscale(image->size, .5), trunc);
-	const int		min = ft_min(image->size.x, image->size.y);
-	const double	radius = (double)(min - !(min & 0b1)) / 2;
-
-	image_clean(image);
-	image_draw_circle(image, colour, center, radius);
+	i = 0;
+	while (++i <= radius)
+		image_draw_circle(image, colour, center, i);
 }
--- srcs/minimap/map_layers_player.c
+++ srcs/minimap/map_layers_player.c
@@ -11,39 +11,6 @@
 /* ************************************************************************** */
 #include "minimap.h"
 
-// t_point	point_multiply(const t_point a, const t_point b)
-// {
-// 	const t_point	point = {
-// 		.x = a.x * b.x,
-// 		.y = a.y * b.y
-// 	};
-
-// 	return (point);
-// }
-
-// static void	layer_player_delta(t_image *layer, const t_player *player)
-// {
-// 	const t_point	map_pos = map_scale_point(player->pos);
-// 	const t_colour	colour = colour_from_percentage(.2, .2, .2, 0);
-// 	t_point			delta;
-// 	t_point			end;
-
-// 	delta = (t_point){
-// 		.x = sqrt((1 + pow(player->dir.y, 2) / pow(player->dir.x, 2))),
-// 		.y = sqrt((1 + pow(player->dir.x, 2) / pow(player->dir.y, 2))),
-// 	};
-// 	delta = map_scale_point(delta);
-// 	if (player->dir.x == 0)
-// 		delta.x = 0;
-// 	if (player->dir.y == 0)
-// 		delta.y = 0;
-// 	delta = point_multiply(delta, point_sign(player->dir));
-// 	end = point_add(map_pos, delta);
-// 	end = point_clamp(end, (t_point){0, 0},
-// 			(t_point){.x = layer->size.x - 1, .y = layer->size.y - 1});
-// 	image_draw_line(layer, colour, map_pos, end);
-// }
-
 void	map_layer_player(t_image *layer, const t_point dir,
 			const t_minimapdata data)
 {
@@ -53,5 +20,5 @@ void	map_layer_player(t_image *layer, const t_point dir,
 		return ;
 	image_draw_line(layer, colour_from_rgba(100, 200, 200, 10),
 		data.player_pos, point_add(data.player_pos, map_scale_point(dir)));
-	image_draw_circle(layer, colour, data.player_pos, 9);
+	image_draw_filled_circle(layer, colour, data.player_pos, 9);
 }
